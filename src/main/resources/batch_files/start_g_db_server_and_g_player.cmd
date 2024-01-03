9fc9e30e4f23413f25c04dfc0b91813009a493e0
mgireesha <mgireesha77@gmail.com>
2024-01-04 00:56:49 +0530

    removed jar

--- src/main/resources/batch_files/start_g_db_server_and_g_player.cmd
+++ src/main/resources/batch_files/start_g_db_server_and_g_player.cmd
@@ -3,6 +3,6 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 
 start java -cp hsqldb-2.7.0.jar org.hsqldb.Server -dbname.0 g_db -database.0 file:g_db
 
-start java -Djdk.util.jar.enableMultiRelease=false -jar C:\Giri\dev\G-Player-SB\target\g-player-1.2.2.jar
+start java -Djdk.util.jar.enableMultiRelease=false -jar C:\Giri\dev\G-Player-SB\target\g-player-2.3.2.jar
 
 #start chrome http://localhost:8085/#/music
